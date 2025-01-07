de1610d0e016b11d97ddb84d87efd573e026a42f
ndihoeric <endihokubwayo@asystresources.com>
2025-01-07 08:38:05 +0200

    resolve of some error in build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div >
       <main >
-        <h1>Bonjour c'est Eric</h1>
+        <h1>Bonjour Eric</h1>
       
       </main>
-      <footer >
-       
-      </footer>
     </div>
   );
 }
